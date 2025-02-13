31df0c47bdaf84229dcab830b4000f42aed9822e
saichiluverupbc <sai.chiluveru@pbcsolutions.ca>
2025-02-13 16:23:40 -0500

    Revert "IVF-216 & 217: Addressing the bugs (#45)"
    This reverts commit d2d4993794887b7efa4b08b57dd44ec1fd160d96.

--- IVF Portal App/force-app/main/default/lwc/caseSubmitButton/caseSubmitButton.js
+++ IVF Portal App/force-app/main/default/lwc/caseSubmitButton/caseSubmitButton.js
@@ -13,7 +13,7 @@ export default class CaseSubmitButton extends LightningElement {
   wiredCaseStatus({ data, error }) {
     if (data) {
       
-      this.isDisabled = !(data === 'Draft' || data === 'Changes Requested' || data === 'Submitted');
+      this.isDisabled = !(data === 'Draft' || data === 'Changes Requested');
     } else if (error) {
       console.error('Error fetching Case status:', error);
     }
